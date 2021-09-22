e000e4688c90546dc9272d857e984112c6cb8956
sarbesh <sarbeshsarkarsks@gmail.com>
2021-09-22 21:37:17 +0530

    Disable CSRF validation

--- src/main/java/com/sarbesh/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/sarbesh/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,9 @@ package com.sarbesh.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,16 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
 	}
 
 }
